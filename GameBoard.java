d911b70113a5a1c7ac1c2faff122a5005c1c27e8
Nyasha Sweto <n.sweto@lancaster.ac.uk>
2026-04-28 12:59:51 +0100

    now setting non full screen frame

--- GameBoard.java
+++ GameBoard.java
@@ -8,6 +8,7 @@ public class GameBoard {
     public static void main(String[]args){
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame setSize(450,450);//width n height
     }
 
 
