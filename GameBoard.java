560556fde5f3fca19d8cc5126452a6c057f82589
Nyasha Sweto <n.sweto@lancaster.ac.uk>
2026-04-28 12:52:03 +0100

    testing commit added a frame for gui

--- GameBoard.java
+++ GameBoard.java
@@ -1,7 +1,16 @@
 import java.awt.*;
 import java.swing.*;
 
+import javax.swing.JFrame;
+
 
 public class GameBoard {
+    public static void main(String[]args){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    
     
 }
